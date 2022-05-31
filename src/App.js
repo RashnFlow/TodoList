a8656ca08d50ccd63ebb8fd8706a0a46051e3730
RashnFlow <62694562+RashnFlow@users.noreply.github.com>
2022-05-31 09:52:47 +0300

    feat: React with Redux Toolkit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './App.css';
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+import { useDispatch } from 'react-redux';
+
+import { addTodo } from './store/todoSlice';
+
+const App = () => {
+  const [text, setText] = useState('');
+  const dispatch = useDispatch();
+
+  const addTask = () => {
+    dispatch(addTodo({text}))
+    setText('')
+  }
+
+  return (
+    <div className="App">
+      <InputField text={text} handleInput={setText} handleSubmit={addTask} />
+
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
